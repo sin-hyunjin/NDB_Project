2c8c8bee4d4de33522dcce4e3bf1986a53de032e
Hyun jin <blackduvet52@gmail.com>
2024-09-01 01:39:20 +0900

    라우터 파일 추가: 사용자 프로필 및 질문 목록 기능
    - 사용자 프로필 페이지를 위한 라우터를 추가 - 데이터베이스에서 사용자 정보를 조회하고 페이지에 렌더링

---
+++ api/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const session = require("express-session");
+const path = require("path");
+
+const app = express();
+
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Nunjucks 설정
+app.set("view engine", "html");
+nunjucks.configure(path.join(__dirname, "../views"), {
+  express: app,
+  watch: true,
+  noCache: true,
+});
+
+// Body-parser 설정
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// 세션 설정
+app.use(
+  session({
+    secret: "secretkey", // 암호화 키
+    resave: false, // 요청이 들어왔을 때 세션을 저장할지 여부
+    saveUninitialized: true, // 초기화되지 않은 세션을 저장할지 여부
+    cookie: { secure: false }, // HTTPS를 사용할 경우 true로 설정
+  })
+);
+
+// 라우트 설정
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views", "index.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views", "login.html"));
+});
+
+app.get("/mypage/reVeiw", (req, res) => {
+  res.sendFile(path.join(__dirname, "../views", "reviewNote.html"));
+});
+
+app.listen(3003, () => {
+  console.log("서버가 3003번 포트에서 실행 중입니다.");
+});
+
+const loginRouter = require("../router/login");
+app.use(loginRouter);
+const indexRouter = require("../router/codeExam");
+app.use(indexRouter);
+const mypageRouter = require("../router/mypage");
+app.use("/mypage", mypageRouter);
+const codeIframeRouter = require("../router/index");
+app.use(codeIframeRouter);
+
+module.exports = app;
